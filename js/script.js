7f9ffc96bb1455b2d7546c5f6b673dfa10c371a7
sinzoouwosasageyo <limshout67@gmail.com>
2025-02-20 15:26:13 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,57 @@
+const gnb_swiper = new Swiper("#gnb",{
+	wrapperClass:"menu",
+	slideClass:"btn",
+	slidesPerView:"auto", //버튼의 갯수만큼 설정
+});
+
+const main_swiper = new Swiper("main",{
+	wrapperClass:"container",
+	slideClass:"area",
+	direction:"vertical",
+	speed:800,
+	mousewheel:{
+		enabled:true,
+	},
+	thumbs:{
+		swiper:gnb_swiper,
+		slideThumbActiveClass:"active",
+	},
+	pagination:{
+		el:".pager",
+		bulletActiveClass:'active',
+		clickable:true,
+	},
+	navigation: {
+    nextEl: '.next',
+    prevEl: '.prev',
+		disabledClass:'hidden',
+  },
+});
+
+/* 모달 윈도우 제어 */
+// 1. DOM 객체 만들기
+var modal = document.getElementById("modal");
+var btns = document.querySelectorAll(".EMW_btn");	//여러개의 버튼 -> 배열
+var iframe = modal.querySelector("iframe");
+
+console.log(btns);
+
+// 2. 버튼 배열에서 버튼 하나하나 지정하기
+btns.forEach(function(btn){
+	// 3. 버튼 이벤트 설정
+	btn.addEventListener("click",function(e){
+		e.preventDefault();	// a 태그 본래의 링크 기능을 동작하지 않게함
+		// 4. 모달에 클래스 부여
+		modal.classList.add("active");
+		// 5. href 값 가져오기
+		var href = this.getAttribute("href");
+		// 6. iframe 설정하기
+		iframe.src = href;
+	});		
+});
+
+// 닫기
+modal.addEventListener("click",function(){
+	modal.classList.remove("active");
+	iframe.src = "works.blank.html";	//원래 상태인 blank 페이지로 바꿈
+});
